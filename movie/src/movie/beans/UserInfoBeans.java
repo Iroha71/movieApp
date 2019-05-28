248fafe3d54fc3e6cc434bdbf1738646214275e1
ken2230 <1701157@st.asojuku.ac.jp>
2019-05-28 14:52:04 +0900

    add:DAO model cont

---
+++ movie/src/movie/beans/UserInfoBeans.java
@@ -0,0 +1,23 @@
+package movie.beans;
+
+import java.io.Serializable;
+
+public class UserInfoBeans implements Serializable {
+
+	private String name;
+	private String mail;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getMail() {
+		return mail;
+	}
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+
+
+}

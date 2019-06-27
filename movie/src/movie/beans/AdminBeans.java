0dbbde6ab4bfecc8d750fb1ca59fd61b4a4f37fc
minoru1027 <1701126@st.asojuku.ac.jp>
2019-06-27 14:23:57 +0900

    add:AdminBeans追加

--- movie/src/movie/beans/AdminBeans.java
+++ movie/src/movie/beans/AdminBeans.java
@@ -1,5 +1,32 @@
 package movie.beans;
 
 public class AdminBeans {
-
+	 int adminNumber;
+	 String adminMail;
+	 String adminName;
+	 String adminPass;
+	public int getAdminNumber() {
+		return adminNumber;
+	}
+	public void setAdminNumber(int adminNumber) {
+		this.adminNumber = adminNumber;
+	}
+	public String getAdminMail() {
+		return adminMail;
+	}
+	public void setAdminMail(String adminMail) {
+		this.adminMail = adminMail;
+	}
+	public String getAdminName() {
+		return adminName;
+	}
+	public void setAdminName(String adminName) {
+		this.adminName = adminName;
+	}
+	public String getAdminPass() {
+		return adminPass;
+	}
+	public void setAdminPass(String adminPass) {
+		this.adminPass = adminPass;
+	}
 }
